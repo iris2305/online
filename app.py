6cc6660f5fd980fa94afbe8068c2d136c9ef888c
iris2305 <79356581+iris2305@users.noreply.github.com>
2021-02-20 17:11:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,51 @@ app = Flask(__name__)
 app.config.from_pyfile('settings.py')
 # add some comments
 ###### Pages
+## Login
+from pages.Login.Login import Login
+app.register_blueprint(Login)
 
-## login
-from pages.login.login import login
-app.register_blueprint(login)
+## Login
+from pages.Login.Login import Login
+app.register_blueprint(Login)
+
+## Solution
+from pages.Solution.Solution import Solution
+
+app.register_blueprint(Solution)
+
+
+## Survey
+from pages.Survey.Survey import Survey
+
+app.register_blueprint(Survey)
+
+
+## Task
+from pages.Task.Task import Task
+
+app.register_blueprint(Task)
+
+
+## TermsOfUse
+from pages.TermsOfUse.TermsOfUse import TermsOfUse
+
+app.register_blueprint(TermsOfUse)
+
+## Thanks
+from pages.Thanks.Thanks import Thanks
+
+app.register_blueprint(Thanks)
+
+## Login
+from pages.Login.Login import Login
+app.register_blueprint(Login)
+
+## Video
+from pages.Video.Video import Video
+app.register_blueprint(Video)
+
+
+## Page error handlers
+from pages.Error.Error import Error
+app.register_blueprint(Error)
