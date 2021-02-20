140eb8b0a503082d252640a5703c50e359910403
iris2305 <79356581+iris2305@users.noreply.github.com>
2021-02-20 15:41:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,7 @@ app.config.from_pyfile('settings.py')
 
 ###### Pages
 ## Login
-from pages.Login.Login import homepage
-app.register_blueprint(Login)
+
+@app.route('/')
+def Login():
+    return render_template('Login.html', foo=42)
