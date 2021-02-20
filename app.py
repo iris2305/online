8c8aac20573733ea574248a6e4036eb2bbc4e779
iris2305 <79356581+iris2305@users.noreply.github.com>
2021-02-20 15:38:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask
 
+
 ###### App setup
 app = Flask(__name__)
-app.secret_key = 'a72389dda4d674'
 app.config.from_pyfile('settings.py')
 
 ###### Pages
 ## Login
-@app.route('/')
-def login():
-    return render_template('login.html', name=name)
+from pages.Login.Login import homepage
+app.register_blueprint(Login)
