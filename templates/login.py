27cc537ddac2ec52848a619b58bad759d9966349
iris2305 <79356581+iris2305@users.noreply.github.com>
2021-02-21 22:04:23 +0200

    test

--- templates/login.py
+++ templates/login.py
@@ -1,11 +1,15 @@
 from flask import Blueprint, render_template, redirect, url_for, session
 
 # login blueprint definition
-login = Blueprint('login', __name__, static_folder='static', static_url_path='/login',
-                     template_folder='templates')
-
+# login = Blueprint('login', __name__, static_folder='static', static_url_path='/login',
+#                      template_folder='templates')
+login = Blueprint('login', __name__)
 
 # Routes
 @login.route('/')
-def index():
+def get_login():
     return "hello world"
+
+@login.route('/test')
+def test_login():
+    return "hello test"
