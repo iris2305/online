19fda833afcf6e99d2b1b521f36d47b790a9082c
iris2305 <79356581+iris2305@users.noreply.github.com>
2021-02-21 21:59:23 +0200

    test llogi router

---
+++ templates/login.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template, redirect, url_for, session
+
+# login blueprint definition
+login = Blueprint('login', __name__, static_folder='static', static_url_path='/login',
+                     template_folder='templates')
+
+
+# Routes
+@login.route('/')
+def index():
+    return "hello world"
