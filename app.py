5163b7b04e5c5deb8bfa4940aba177fb5341522c
iris2305 <79356581+iris2305@users.noreply.github.com>
2021-02-25 20:01:18 +0200

    connect to db

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask
 from pages.login.login import login
 # from pages.be.be import be
-# import psycopg2
-# import settings
+import psycopg2
+import settings
 #
-# EVENT_INSERT =  '''
-# INSERT INTO events_logs
-# (user_code, page_num, duration, last_updated)
-# VALUES('iris', 2, 10, now())
-# on conflict on constraint events_logs_un  do update set
-# duration = 20 ,last_updated = now()
-#
-# '''
+EVENT_INSERT =  '''
+INSERT INTO events_logs
+(user_code, page_num, duration, last_updated)
+VALUES('iris', 2, 10, now())
+on conflict on constraint events_logs_un  do update set
+duration = 20 ,last_updated = now()
+
+'''
 
 ###### App setup
 app = Flask(__name__)
@@ -25,11 +25,11 @@ app.register_blueprint(login)
 # Routes
 @app.route('/be')
 def be_index():
-    # connection = psycopg2.connect(user=settings.DB['user'], password=settings.DB['password'],
-    #                               host=settings.DB['host'], post=settings.DB['post'],
-    #                               database=settings.DB['database'])
-    # cursor = connection.cursor()
-    # cursor.execute(EVENT_INSERT)
+    connection = psycopg2.connect(user=settings.DB['user'], password=settings.DB['password'],
+                                  host=settings.DB['host'], post=settings.DB['post'],
+                                  database=settings.DB['database'])
+    cursor = connection.cursor()
+    cursor.execute(EVENT_INSERT)
     return "helo world!"
 
 
