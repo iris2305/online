6f22a670bf076498fc1126ffecef40c2d2ad5d38
iris2305 <79356581+iris2305@users.noreply.github.com>
2021-02-25 21:20:33 +0200

    sedning js

--- pages/login/static/js/login.js
+++ pages/login/static/js/login.js
@@ -1,6 +1,20 @@
 console.log("LOGIN js loaded...");
 //took it from :https://www.youtube.com/watch?v=QukFW1qymSA&ab_channel=iEatWebsites
 
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+
+session = sessionStorage.getItem("user_session");
+if(!session){
+    session = uuidv4()
+    sessionStorage.setItem("user_session",session)
+}
+
 var duration = 0;
     var page = 1;
     console.log("Starting sending loop")
@@ -13,6 +27,6 @@ setInterval(function(){
             console.log("success insert wow")
         }
     }
-    xhttp.open("get","https://finalprojectonlineinterviews.herokuapp.com/be/iris2/"+page+"/"+duration);
+    xhttp.open("get","https://finalprojectonlineinterviews.herokuapp.com/be/iris2/"+session+"/"+page+"/"+duration);
     xhttp.send();
 },2000);
