3e26a921583e5dceaf3124d5f784a4872597db11
Kobi Dalal <kobidalal1@gmail.com>
2021-02-28 21:27:09 +0200

    moved be to blueprint

--- app.py
+++ app.py
@@ -1,17 +1,6 @@
 from flask import Flask
 from pages.login.login import login
-# from pages.be.be import be
-import psycopg2
-import settings
-#
-EVENT_INSERT =  '''
-INSERT INTO events_logs
-(user_code, page_num, duration,session, last_updated)
-VALUES('{0}', {1}, {2},'{3}', now())
-on conflict on constraint events_logs_un  do update set
-duration = {2} ,last_updated = now()
-
-'''
+from pages.be.be import be
 
 ###### App setup
 app = Flask(__name__)
@@ -22,17 +11,9 @@ app.config.from_pyfile('settings.py')
 
 
 app.register_blueprint(login)
+app.register_blueprint(be)
+
 # Routes
-@app.route('/be/<user>/<session>/<page>/<duration>')
-def be_index(user,session, page, duration):
-    sql = EVENT_INSERT.format(user, page , duration, session)
-    connection = psycopg2.connect(user=settings.DB['user'], password=settings.DB['password'],
-                                  host=settings.DB['host'], port=settings.DB['port'],
-                                  database=settings.DB['database'])
-    cursor = connection.cursor()
-    cursor.execute(sql)
-    connection.commit()
-    return "helo world!"
 
 
 #app.register_blueprint(be)
--- pages/be/be.py
+++ pages/be/be.py
@@ -1,29 +1,40 @@
-from flask import Blueprint, render_template, redirect, url_for, session
+from flask import Blueprint, render_template, redirect, url_for, session,request, jsonify
+
 import psycopg2
 import settings
 
 EVENT_INSERT =  '''
 INSERT INTO events_logs
-(user_code, page_num, duration, last_updated)
-VALUES('{0}', 2, 10, now())
-on conflict on constraint events_logs_un  do update set 
-duration = 20 ,last_updated = now()
+(user_code, page_num, duration,session, last_updated)
+VALUES('{0}', {1}, {2},'{3}', now())
+on conflict on constraint events_logs_un  do update set
+duration = {2} ,last_updated = now()
 
 '''
+
 
 from psycopg2 import Error
 # login blueprint definition
 be = Blueprint('be', __name__,  url_prefix="/be")
 
-
+@be.route('/')
+def be_main():
+    return "Hello"
 
 # Routes
-@be.route('/')
-def be_index():
+@be.route('/<user>/<session>/<page>/<duration>')
+def be_index(user,session, page, duration):
+    sql = EVENT_INSERT.format(user, page , duration, session)
     connection = psycopg2.connect(user=settings.DB['user'], password=settings.DB['password'],
-                                  host=settings.DB['host'], post=settings.DB['post'],
+                                  host=settings.DB['host'], port=settings.DB['port'],
                                   database=settings.DB['database'])
     cursor = connection.cursor()
-    cursor.execute(EVENT_INSERT)
-    return "helo world!"
+    cursor.execute(sql)
+    connection.commit()
+    return "hello world!"
 
+
+@be.route('/be_post', methods=['POST'])
+def be_post():
+    data = request.get_json()
+    return jsonify(data)
