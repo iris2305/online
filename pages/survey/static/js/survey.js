c14c25d0b9d2632e3b79e8fb1932c2e673c8196c
iris2305 <79356581+iris2305@users.noreply.github.com>
2021-03-01 19:31:39 +0200

    change

---
+++ pages/survey/static/js/survey.js
@@ -0,0 +1,31 @@
+
+var duration = 0;
+var page = 7;
+var user =sessionStorage.getItem("user_code");
+session = sessionStorage.getItem("user_session");
+if(!session){
+    session = uuidv4()
+    sessionStorage.setItem("user_session",session)
+}else{
+    if(sessionStorage.getItem("page_"+page)){
+        duration = parseInt(sessionStorage.getItem("page_"+page));
+        console.log("starting with duration ",duration)
+    }
+}
+
+
+console.log("Starting sending loop")
+setInterval(function(){
+    console.log("Sending data to server")
+    duration += 2;
+    sessionStorage.setItem("page_"+page,duration.toString());
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function (){
+        if(this.readyState == 4 && this.status == 200){
+            console.log("success insert wow")
+        }
+    }
+    xhttp.open("get","https://finalprojectonlineinterviews.herokuapp.com/be/"+user+"/"+session+"/"+page+"/"+duration);
+    xhttp.send();
+},2000);
