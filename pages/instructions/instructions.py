bf9f0bbc65d3ce82dad7664dc75e27b442410a3a
iris2305 <79356581+iris2305@users.noreply.github.com>
2021-03-02 17:19:32 +0200

    Update instructions.py

--- pages/instructions/instructions.py
+++ pages/instructions/instructions.py
@@ -1,4 +1,4 @@
-# login blueprint definition
+from flask import Blueprint, render_template, redirect, url_for, session
 instructions = Blueprint('instructions', __name__, static_folder='static', static_url_path='/instructions',
                   template_folder='templates')
 
