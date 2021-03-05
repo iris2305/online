130af26f40cd4d3e872a386527db250286b53c85
iris2305 <79356581+iris2305@users.noreply.github.com>
2021-03-05 18:29:09 +0200

    FIX APP

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from pages.instructions.instructions import instructions
 from pages.termsofuse.termsofuse import termsofuse
 from pages.solution.solution import solution
 from pages.survey.survey import survey
-from pages.demo.task import task
 from pages.video.video import video
 from pages.thanks.thanks import thanks
 from pages.demo.demo import demo
@@ -24,7 +23,6 @@ app.register_blueprint(login)
 app.register_blueprint(instructions)
 app.register_blueprint(solution)
 app.register_blueprint(survey)
-app.register_blueprint(task)
 app.register_blueprint(thanks)
 app.register_blueprint(video)
 app.register_blueprint(be)
--- pages/demo/demo.py
+++ pages/demo/demo.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-demo = Blueprint('demo', __name__, static_folder='static', static_url_path='/demo',url_prefix='/demo',
+demo = Blueprint('demo', __name__, static_folder='static', static_url_path='/demo', url_prefix='/demo',
                    template_folder='templates')
 
 
