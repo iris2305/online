9f1214109fc0f6bee00a6cd85601bff4baffefde
iris2305 <79356581+iris2305@users.noreply.github.com>
2021-03-25 19:29:06 +0200

    CHANGE VIDEO

--- pages/solution/static/js/next_task.js
+++ pages/solution/static/js/next_task.js
@@ -1,9 +1,33 @@
 
 
 let videos = { //put task according to type video-> 1:task 1, 2: task 2, 3: task 3
-    '/static/images/VIDEO1.mp4': 1,
-    '/static/images/VIDEO2.mp4': 2,
-    '/static/images/VIDEO3.mp4': 3
+    '/static/images/A1A.mp4': 1,
+    '/static/images/A1B.mp4': 1,
+    '/static/images/A1C.mp4': 1,
+    '/static/images/A2A.mp4': 2,
+    '/static/images/A2B.mp4': 2,
+    '/static/images/A2C.mp4': 2,
+    '/static/images/A3A.mp4': 3,
+    '/static/images/A3B.mp4': 3,
+    '/static/images/A3C.mp4': 3,
+    '/static/images/I1A.mp4': 1,
+    '/static/images/I1B.mp4': 1,
+    '/static/images/I1C.mp4': 1,
+    '/static/images/I2A.mp4': 2,
+    '/static/images/I2B.mp4': 2,
+    '/static/images/I2C.mp4': 2,
+    '/static/images/I3A.mp4': 3,
+    '/static/images/I3B.mp4': 3,
+    '/static/images/I3C.mp4': 3,
+    '/static/images/T1A.mp4': 1,
+    '/static/images/T1B.mp4': 1,
+    '/static/images/T1C.mp4': 1,
+    '/static/images/T2A.mp4': 2,
+    '/static/images/T2B.mp4': 2,
+    '/static/images/T2C.mp4': 2,
+    '/static/images/T3A.mp4': 3,
+    '/static/images/T3B.mp4': 3,
+    '/static/images/T3C.mp4': 3
     //27 videos of 3 tasks
 }
 debugger
